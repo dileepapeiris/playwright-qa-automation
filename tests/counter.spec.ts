4f49a254d4d221b58b3876e7837cf2926bae26ac
K.Dileepa Thushan Peiris <dileepapeiris5@gmail.com>
2026-06-16 11:15:23 +0530

    test: verify clicking Increase 3 times shows Count: 3

--- tests/counter.spec.ts
+++ tests/counter.spec.ts
@@ -11,3 +11,12 @@ test("clicking Increase adds 1", async ({ page }) => {
   await page.getByRole("button", { name: "Increase" }).click();
   await expect(page.getByTestId("count-display")).toHaveText("Count: 1");
 });
+
+test("clicking Increase 3 times shows Count: 3", async ({ page }) => {
+  await page.goto("/");
+  const increaseBtn = page.getByRole("button", { name: "Increase" });
+  await increaseBtn.click();
+  await increaseBtn.click();
+  await increaseBtn.click();
+  await expect(page.getByTestId("count-display")).toHaveText("Count: 3");
+});
